5a52a5c19d3438ae0dd7e101083dd60011061c0d
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2022-05-20 07:33:27 -0300

    hero and nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+
 export default function App(){
     return(
         <div>
-            <h1>App</h1>
+           <Navbar/>
+           <Hero/>
         </div>
     )
 } 
---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import hero from "../images/hero.png"
+
+export default function Hero(){
+    return(
+        <div className="hero">
+           <img src={hero}></img>
+        </div>
+    )
+} 
